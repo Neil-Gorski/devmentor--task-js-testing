1f2dba4195720aff72edd6da1986fdd8f8a113a9
Neil Gorski <neil.gorski@hotmail.ch>
2025-08-30 21:48:19 +0200

    add test meta data is correct

--- tests/GitHubSDK.test.js
+++ tests/GitHubSDK.test.js
@@ -24,4 +24,18 @@ describe("GitHubSDK - fetch repos data", () => {
   test("array is not empty", () => {
     expect(myRepos.length).toBeGreaterThan(0);
   });
+  test("meta data is correct", () => {
+    expect(myRepos[0]).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        node_id: expect.any(String),
+        name: expect.any(String),
+        owner: expect.objectContaining({
+          login: expect.any(String),
+        }),
+        private: expect.any(Boolean),
+        fork: expect.any(Boolean),
+      })
+    );
+  });
 });
