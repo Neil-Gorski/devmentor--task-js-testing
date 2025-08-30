2b67cfb345bcd141b96c0d329a9892c08a59d8f2
Neil Gorski <neil.gorski@hotmail.ch>
2025-08-30 21:59:47 +0200

    add getListOfRepos method

--- src/GitHubSDK.js
+++ src/GitHubSDK.js
@@ -18,4 +18,19 @@ export default class GitHubSDK {
     const data = await res.json();
     return data;
   }
+
+  async getListOfRepos(owner) {
+    const url = `${this.baseURL}/users/${owner}/repos?per_page=100`;
+    const fetchObj = {
+      method: "GET",
+      headers: {
+        accept: "application/vnd.github+json",
+        Authorization: `Bearer ${this.token}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    };
+    const res = await fetch(url, fetchObj);
+    const data = await res.json();
+    return data;
+  }
 }
