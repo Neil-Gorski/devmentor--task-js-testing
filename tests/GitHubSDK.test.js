dd2ae8a99f8def3121def95c9b5c0d21b5c6118b
Neil Gorski <neil.gorski@hotmail.ch>
2025-08-31 00:07:57 +0200

    Creat CRUD tests

--- tests/GitHubSDK.test.js
+++ tests/GitHubSDK.test.js
@@ -66,4 +66,81 @@ describe("GitHubSDK - fetch my repos data", () => {
       );
     });
   });
+  describe("GitHubSDK - CRUD repo", () => {
+    let gh;
+    const payload = {
+      name: "GitHubSDK - Test repo",
+      description:
+        "This repository serves as a testing environment for the GitHub REST API. It is used to test and verify the creation of repositories via API calls.",
+      private: false,
+      visibility: "public",
+    };
+    beforeAll(async () => {
+      gh = new GitHubSDK("Neil-Gorski", TOKEN);
+    });
+    test("Create a repo", async () => {
+      const resp = await gh.createRepo(payload);
+      expect(resp.status).toBe(201);
+      expect(resp.error).toBe(null);
+      expect(resp.data).toEqual(
+        expect.objectContaining({
+          name: payload.name,
+          description: payload.description,
+          private: payload.private,
+          visibility: payload.visibility,
+        })
+      );
+    });
+    test("fails create same repo again", async () => {
+      const resp = await gh.createRepo(payload);
+      expect(resp.status).toBe(422);
+      expect(resp.error).toBe("Error: Repo with this name allready exists.");
+    });
+    test("find created repo", async () => {
+      const resp = await gh.getRepo(payload.name);
+      expect(resp.status).toBe(200);
+      expect(resp.error).toBe(null);
+      expect(resp.data).toEqual(
+        expect.objectContaining({
+          name: payload.name,
+          description: payload.description,
+          private: payload.private,
+          visibility: payload.visibility,
+        })
+      );
+    });
+    test("fails to find non existing repo", async () => {
+      const resp = await gh.getRepo(payload.name + "unknown");
+      expect(resp.status).toBe(404);
+      expect(resp.error).toBe(`Error: Can not find non existing repo.`);
+      expect(resp.data).toBe(null);
+    });
+    test("update repo description", async () => {
+      payload.description =
+        "This repository serves as a testing environment for the GitHub REST API. It is used to test and verify the creation of repositories via API calls. Description has been updated.";
+      const resp = await gh.updateRepo(payload.name, payload);
+      expect(resp.status).toBe(200);
+      expect(resp.error).toBe(null);
+      expect(resp.data).toEqual(
+        expect.objectContaining({
+          name: payload.name,
+          description: payload.description,
+          private: payload.private,
+          visibility: payload.visibility,
+        })
+      );
+    });
+    test("delete repo", async () => {
+      const resp = await gh.deleteRepo(payload.name);
+      expect(resp.status).toBe(204);
+      expect(resp.error).toBe(null);
+      expect(resp.data).toBe(null);
+    });
+    test("error trying delete non existing repo", async () => {
+      const resp = await gh.deleteRepo(payload.name);
+      expect(resp.status).toBe(404);
+      expect(resp.error).toBe("Error: Can not delete non existing repo.");
+      expect(resp.data).toBe(null);
+    });
+  });
 });
