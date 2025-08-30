e3531bf406d681e19858421cf13952e0a89d7ddc
Neil Gorski <neil.gorski@hotmail.ch>
2025-08-30 21:57:22 +0200

    add test GitHubSDK - fetch devmentor-pl repos data

--- tests/GitHubSDK.test.js
+++ tests/GitHubSDK.test.js
@@ -11,21 +11,21 @@ describe("GitHubSDK - constructor", () => {
   });
 });
 
-describe("GitHubSDK - fetch repos data", () => {
+describe("GitHubSDK - fetch my repos data", () => {
   let gh;
-  let myRepos;
+  let repos;
   beforeAll(async () => {
     gh = new GitHubSDK("Neil-Gorski", TOKEN);
-    myRepos = await gh.getListOfMyRepos();
+    repos = await gh.getListOfMyRepos();
   });
   test("returns an arry", async () => {
-    expect(Array.isArray(myRepos)).toBe(true);
+    expect(Array.isArray(repos)).toBe(true);
   });
   test("array is not empty", () => {
-    expect(myRepos.length).toBeGreaterThan(0);
+    expect(repos.length).toBeGreaterThan(0);
   });
   test("meta data is correct", () => {
-    expect(myRepos[0]).toEqual(
+    expect(repos[0]).toEqual(
       expect.objectContaining({
         id: expect.any(Number),
         node_id: expect.any(String),
@@ -37,5 +37,33 @@ describe("GitHubSDK - fetch repos data", () => {
         fork: expect.any(Boolean),
       })
     );
+  });
+  describe("GitHubSDK - fetch devmentor-pl repos data", () => {
+    let gh;
+    let repos;
+    beforeAll(async () => {
+      gh = new GitHubSDK("Neil-Gorski", TOKEN);
+      repos = await gh.getListOfRepos("devmentor-pl");
+    });
+    test("returns an arry", async () => {
+      expect(Array.isArray(repos)).toBe(true);
+    });
+    test("array is not empty", () => {
+      expect(repos.length).toBeGreaterThan(0);
+    });
+    test("meta data is correct", () => {
+      expect(repos[0]).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          node_id: expect.any(String),
+          name: expect.any(String),
+          owner: expect.objectContaining({
+            login: expect.any(String),
+          }),
+          private: expect.any(Boolean),
+          fork: expect.any(Boolean),
+        })
+      );
+    });
   });
 });
